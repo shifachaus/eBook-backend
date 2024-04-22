8d88e2fe480e32714c8a6ebe6ac43cab0127d1f2
shifachaus <chausshifa804@gmail.com>
2024-04-22 14:16:51 +0530

    add list book endpoint

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -181,4 +181,14 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
 
   res.json(updatedBook);
 };
-export { createBook, updateBook };
+
+const listBooks = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const book = await bookModel.find();
+    res.json(book);
+  } catch (err) {
+    return next(createHttpError(500, "Error while getting a book"));
+  }
+};
+
+export { createBook, updateBook, listBooks };
