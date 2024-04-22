e29096019d42f72c7038e58ca03ef7d356139e0f
shifachaus <chausshifa804@gmail.com>
2024-04-22 11:39:12 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,49 @@
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "path";
+import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  const {} = req.body;
-  res.json({ message: "ok" });
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+  // 'application/pdf'
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+
+    res.status(201).json({ message: "ok" });
+  } catch (err) {
+    console.log(err);
+    return next(createHttpError(500, "Error while uploading the files."));
+  }
 };
 
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
